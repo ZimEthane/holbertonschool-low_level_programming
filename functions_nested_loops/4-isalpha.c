06ba853d13d754f055470ea47980118d507dcf99
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-21 13:40:46 +0000

    fix change name to function

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _islower - check the code.
+ * _isalpha - check the code.
  * @c: The character to be checked
  *
  * Return: Always 0.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 if (c >= 'A' && c <= 'Z' && c >= 'a' && c <= 'z')
 return (1);
