b30136fa6f9b07cc661273490e32030a1e69d6b1
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-21 13:52:25 +0000

    feat : create a function that prints the sign of a number.

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code.
+ * @n: The character to be checked
+ *
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
