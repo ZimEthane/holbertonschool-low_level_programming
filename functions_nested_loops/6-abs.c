b64917aea475612c9f519e33bf5a317236144000
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-22 07:21:50 +0000

    feat : create  a function that computes the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code.
+ * @n: The character to be checked
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+n = (n < 0 )? -n : n ;
+return (n);
+}
