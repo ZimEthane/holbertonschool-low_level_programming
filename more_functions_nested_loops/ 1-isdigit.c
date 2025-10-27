75aed14ed16680cc274f76503706ad0066633193
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 09:30:01 +0000

    feat : create a function that checks for a digit (0 through 9)

---
+++ more_functions_nested_loops/ 1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - Entry point
+ * @c: variable tester
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
