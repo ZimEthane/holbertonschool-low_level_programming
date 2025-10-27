72380424e2486651551978f269bce4c66d9793ea
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 10:42:52 +0000

    fix : modify condition

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,16 +12,16 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
-
-			if (n <= 0)
-			{
-				_putchar('\n');
-			}
 		}
 		_putchar('\\');
 		_putchar('\n');
