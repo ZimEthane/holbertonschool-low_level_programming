3a668f74bd2f654b67cf66622abf05b65ffb92e0
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 09:35:33 +0000

    feat : create a function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - function multiplies two integers
+ * @a: variable tester
+ * @b: variable tester
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
