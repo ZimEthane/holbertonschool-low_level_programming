b21e240409d7200ed0f1210ec5f71fe0e9deddbe
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 08:58:35 +0000

    fix : include

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * int _isupper(int c) - Entry point
@@ -9,20 +9,12 @@
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-_putchar("%d", c);
-_putchar(':');
-_putchar(' ');
-_putchar('1');
-return (1);
-}
-else
-{
-_putchar("%d", c);
-_putchar(':');
-_putchar(' ');
-_putchar('0');
-return (0);
-}
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
