54c4f19048f2cea6c0ed8005a6c3d7f9481a09ae
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 14:30:24 +0000

    feat : create  a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * @n: pointer to an integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
