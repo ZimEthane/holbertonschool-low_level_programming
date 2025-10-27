55230b676dfd9382306001010f3f73e190ac5cbd
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 08:52:14 +0000

    fix : change '' putchar

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -11,7 +11,7 @@ int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 {
-_putchar('%d', c);
+_putchar("%d", c);
 _putchar(':');
 _putchar(' ');
 _putchar('1');
@@ -19,7 +19,7 @@ return (1);
 }
 else
 {
-_putchar('%d', c);
+_putchar("%d", c);
 _putchar(':');
 _putchar(' ');
 _putchar('0');
