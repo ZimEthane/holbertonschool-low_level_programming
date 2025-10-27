2bc20f1583609c2f539d81638e74b846cc5068a4
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 09:41:03 +0000

    feat : create a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - function multiplies two integers
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+}
