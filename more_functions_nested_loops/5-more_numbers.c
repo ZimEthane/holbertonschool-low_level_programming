5f13fb8ffa429543a513f92e623d94ea7e634e95
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 10:07:29 +0000

    feat : create a function that prints 10 times the numbers, from 0 to 14, followed by a new line

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - function multiplies two integers
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar('0' + j);
+		}
+		_putchar('\n');
+	}
+}
