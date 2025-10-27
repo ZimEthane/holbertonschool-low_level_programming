ade8ab878099be327e4eca337a573156d96a7c0f
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 08:51:12 +0000

    fiw : betty

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,28 @@
 #include "main.h"
+
 /**
  * int _isupper(int c) - Entry point
  * @c - variable tester
  *
  * Return: Always 0 (Success)
  */
+
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        _putchar('1');
-        return (1);
-    }
-
-    else
-    {
-        _putchar('0');
-        return (0);
-    }
+if (c >= 'A' && c <= 'Z')
+{
+_putchar('%d', c);
+_putchar(':');
+_putchar(' ');
+_putchar('1');
+return (1);
+}
+else
+{
+_putchar('%d', c);
+_putchar(':');
+_putchar(' ');
+_putchar('0');
+return (0);
+}
 }
