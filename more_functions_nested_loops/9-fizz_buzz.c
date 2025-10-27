465938c54849a981b506508514b1d8dd4be80366
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-27 13:41:49 +0000

    fix : change code

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,24 +13,30 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-
-		if (i % 15 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
-		printf(" ");	
 	}
+	putchar('\n');
 	return (0);
 }
