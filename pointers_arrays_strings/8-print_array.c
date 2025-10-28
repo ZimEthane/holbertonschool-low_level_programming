1d74707f24c56a215a11e437700b73ed0d26de6c
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-28 13:53:31 +0000

    feat : ajouter la fonction print_array pour imprimer les éléments d'un tableau d'entiers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements of an array of integers,
+ * followed by a new line
+ * @a: pointer to an array of integers
+ * @n: number of elements of the array to be printed
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i ++)
+	{
+		printf("%d, ", a[i]);
+	}
+}
