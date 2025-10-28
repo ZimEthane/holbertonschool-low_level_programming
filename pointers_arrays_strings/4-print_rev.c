6e675c9a594a3acdba305cc81c99b625b02ca3d4
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-28 09:06:32 +0000

    feat : create a function that prints a string, in reverse, followed by a new line

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - function that prints a string, in reverse, followed by a new line
+ * @s: pointer to a string
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int lenght = 0;
+	int i;
+
+	while (s[lenght] != '\0')
+	{
+		lenght++;
+	}
+
+	for (i = lenght ; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
