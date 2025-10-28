428f478d50f6816b4b32f22224c0709e78e162ac
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-28 13:04:18 +0000

    fix : remplace for betty code

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,27 +14,27 @@ void puts_half(char *str)
 {
 	int lenght = 0;
 
-	int j ;
+	int j;
 
 	while (str[lenght] != '\0')
 	{
 		lenght++;
 	}
-	
+
 	if (lenght % 2 == 0)
 	{
 		for (j = lenght / 2; str[j] != '\0'; j++)
 		{
-			putchar(str[j]);
+			_putchar(str[j]);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
 		for (j = (lenght + 1) / 2; str[j] != '\0'; j++)
 		{
-			putchar(str[j]);
+			_putchar(str[j]);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
