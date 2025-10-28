be1e32b037de5482e5273d9f77665c14b2672018
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-28 10:23:03 +0000

    feat : ajouter la fonction puts2 pour imprimer chaque deuxième caractère d'une chaîne

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - function that prints every other character of a string,
+ * starting with the first character, followed by a new line
+ * @str: pointer to a string
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void puts2(char *str)
+{
+	int i, j, lenght;
+
+	lenght = 0;
+
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+
+	j = lenght - 1;
+
+	for (i = 0; i < j; i + 2)
+	{
+		_putchar(str[i]);
+	}
+}
