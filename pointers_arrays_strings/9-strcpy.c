d4fddff2a5082c55660f3ba3cd95d693455909ca
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-28 14:53:41 +0000

    feat : ajouter la fonction _strcpy pour copier des chaînes de caractères

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest.
+ * @param dest: pointer to the destination array where the content is to be copied
+ * @param src: pointer to the source of data to be copied
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+}
