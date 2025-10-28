5d943636101a555b9b309e3dcacdb457d185d6be
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-28 08:39:08 +0000

    feat : create  a function that prints a string, followed by a new line

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: pointer to a string
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	int i;
+	i = 0;
+
+	while (str[i] != '/0')
+	{
+		_putchar(str[i]);
+		_putchar('\n');
+		i++;
+	}
+}
