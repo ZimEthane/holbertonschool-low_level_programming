fbcfaf7b409dbfd771feed06c1daa58de71bb97c
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-29 13:04:06 +0000

    feat : create  a function that changes all lowercase letters of a string to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper -	 function that reverses a string
+ * @s: pointer to a string
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -31,4 +31,6 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
+char *string_toupper(char *s);
+
 #endif
