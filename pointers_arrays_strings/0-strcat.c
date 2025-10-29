d897e043a0b4054d302a0b7748def9c7e02398e6
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-29 09:39:15 +0000

    feat : create  a function that concatenates two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
