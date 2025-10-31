a3d909178e548dd0b5c3bdb392a5471bf1b1a717
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-31 09:13:53 +0000

    feat : creation de la loops pour tester chaque caractere de la liste s et creation de la condition pour afficher les nombre de 0 a 9

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,5 +12,19 @@
 
 int _atoi(char *s)
 {
-	
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			_putchar(s[i]);
+		}
+		else if (s[i] == '-')
+		{
+		}
+		else
+		{
+		}
+	}
 }
