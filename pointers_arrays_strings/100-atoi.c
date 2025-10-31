47c0bdaa6dc57447d41964a49aea44a88d2e2f41
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-10-31 10:02:53 +0000

    fix : add condition else if and complete condition if

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,14 +13,20 @@
 int _atoi(char *s)
 {
 	int i;
+	int sign = 1;
+	int result = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= '0' || s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			_putchar(s[i]);
+			result = result * 10 + (s[i] - '0');
+
 		}
-		
+		else if (s[i] == '-')
+		{
+			sign = -sign;
+		}
 	}
 	return (s[i]);
 }
