25229d03ae6776704e662a71920689ec19d7ff10
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-03 08:47:02 +0000

    create a  a function that copies memory area

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: pointer to the memory area
+ * @b: constant byte to fill the memory area
+ * @n: number of bytes to be filled
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
