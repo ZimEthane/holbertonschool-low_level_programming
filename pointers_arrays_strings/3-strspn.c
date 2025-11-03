d2f9bbb267ea11c9c2e6f5af7100a5944c4bc1f0
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-03 09:40:54 +0000

    feat : create a function that gets the length of a prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: pointer to the string to be searched
+ * @accept: pointer to the string containing the characters to match
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+	char *a; 
+
+	count = 0;
+
+	while (*s)
+	{
+		for (a = accept; *a; a++)
+		{
+			if (*s == *a)
+			{
+				count++;
+				break; 
+			}
+		}
+		if (*a == '\0')
+			return (count);
+		s++;
+	}
+}
