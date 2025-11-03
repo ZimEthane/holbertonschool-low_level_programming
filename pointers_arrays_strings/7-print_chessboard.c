fe9eaf5fa57b30d0e76eff665fac62c86cae94db
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-03 10:58:31 +0000

    feat : create a fun a function that prints the chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: pointer to a 2D array representing the chessboard
+ * 
+ * Return: Always 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}
