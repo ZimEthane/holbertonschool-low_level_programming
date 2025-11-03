0dfe6e1ecc057e14162ee6ed1506caa8a37cbc1e
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-03 13:10:10 +0000

    fix : betty code style (tabulation)

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -6,7 +6,7 @@
  * of a square matrix of integers.
  * @a: pointer to the first element of the matrix
  * @size: size of the square matrix
- * 
+ *
  * Return: void
  */
 
@@ -18,7 +18,7 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i * size + i);           
+		sum1 += *(a + i * size + i);
 		sum2 += *(a + i * size + (size - 1 - i));
 	}
 
