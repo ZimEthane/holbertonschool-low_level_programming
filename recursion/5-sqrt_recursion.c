6ae4d749a683c18050eb9e900581f599be302b1d
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-04 09:49:01 +0000

    fix : update remning code

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,16 +3,20 @@
 
 /**
  * _sqrt_helper - helper function to find the square root
- * @n: integer to find the square root of
+ * @n: number to find the square root of
+ * @guess: current guess for the square root
  *
- * Return: square root of n or -1 if not a perfect square
+ * Return: square root of n or -1 if n has no natural square root
  */
-
-int _sqrt_recursion(int n)
+int _sqrt_helper(int n, int guess)
 {
-	if (n < 0)
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	else if (guess * guess > n)
 	{
 		return (-1);
 	}
-	return (_sqrt_helper(n, 0));
+	return (_sqrt_helper(n, guess + 1));
 }
