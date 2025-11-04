62a4d17cbe52168f60d85fc1f6c9bb37ff25b43a
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-04 08:18:57 +0000

    feat : create a function that prints a string, followed by a new line

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string followed by a new line
+ * @s: pointer to a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	return (0);
+}
