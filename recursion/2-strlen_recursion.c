23824b5e0c28818515f894c09f14ace797e2c80d
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-04 08:32:01 +0000

    feat : create a function that returns the length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "../functions_nested_loops/main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: pointer to a string
+ *
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
