8488bab6aee2be22c4ffea2ead991941e621c467
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-04 09:36:20 +0000

    fix : update code

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -14,12 +14,5 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	else
-	{
-		return (_sqrt_helper(n + 1));
-	}
+	return (_sqrt_helper(n, 0));
 }
