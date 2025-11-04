7bd97199995ea735eef3f5f70d36c26d8bebd9d9
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-04 14:28:25 +0000

    fix : change code for remaning

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -12,10 +12,16 @@
 int is_prime_number(int n)
 {
 	if (n <= 1)
+	{
 		return (0);
+	}
 	if (n == 2)
+	{
 		return (1);
+	}
 	if (n % 2 == 0)
+	{
 		return (0);
-	return (is_prime_helper(n, 3));
+	}
+	return (is_prime_number(n - 1) && (n % (n - 1) != 0));	
 }
