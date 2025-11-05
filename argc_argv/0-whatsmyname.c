7feaabb717472ab2f8ea7df27007852bb9b2823a
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-05 08:31:57 +0000

    fix : add description in programme for betty style

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
