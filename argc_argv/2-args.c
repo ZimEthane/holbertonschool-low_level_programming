ab00c145791caf1c21dddc95e3188aed4f7f15be
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-05 08:39:15 +0000

    feat : create a program that prints all arguments it receives

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
