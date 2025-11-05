c31d3d2cb968d3ba1629caea0e7fa94f8b0a4b11
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-05 13:19:32 +0000

    fix : update code add loops for in my code

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,9 +12,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0, num;
+	int i, j, sum = 0;
 
-	if (argc < 2)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
@@ -22,13 +22,18 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		num = atoi(argv[i]);
-		if (num < 0)
+		char *arg = argv[i];
+
+		for (j = 0; arg[j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (arg[j] < '0' || arg[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += num;
+
+		sum += atoi(arg);
 	}
 
 	printf("%d\n", sum);
