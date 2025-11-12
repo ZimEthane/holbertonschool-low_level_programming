55c468b2fab9841c3ea037057739b4583c91dd92
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-12 17:39:17 +0000

    feat : create a function that concatenates two strings and create a other function that allocates memory for an array, using malloc and a function that creates an array of integers

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,5 +13,28 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+	unsigned int total_size, i;
+	unsigned char *byte_ptr;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	byte_ptr = (unsigned char *)ptr;
+	for (i = 0; i < total_size; i++)
+	{
+		byte_ptr[i] = 0;
+	}
+
+	return (ptr);
 }
--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,5 +13,27 @@
 
 int *array_range(int min, int max)
 {
-	
+	int *array;
+	int i, size;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	size = max - min + 1;
+
+	array = (int *)malloc(size * sizeof(int));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = min + i;
+	}
+
+	return (array);
 }
