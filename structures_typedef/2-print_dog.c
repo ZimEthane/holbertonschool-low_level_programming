eabdc1f3cb15065be4cd2bf5cc41f9076f8e55cc
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-13 13:25:09 +0000

    feat : create a function that prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Affiche les informations d'une structure dog
+ * @d: Pointeur vers la structure dog à afficher
+ *
+ * Description: Cette fonction affiche le nom, l'âge et le propriétaire
+ *              du chien contenu dans la structure dog.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("Age: %lf\n", d->age);
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+}
