dbd5bfbbf5b9dbfe04a573714c0d4f1c15cceee8
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-13 13:12:19 +0000

    feat : create a struture dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure qui contient des informations sur un chien.
+ *
+ * @name: nom du chien.
+ * @age: age du chien.
+ * @owner: nom du proprietaire du chien.
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
