3d99ef15897684bdb12308972e782f03f9077c02
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-13 13:43:25 +0000

    feat : create a function that creates a new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - Crée une nouvelle instance de dog_t
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Nom du propriétaire du chien
+ *
+ * Return: Pointeur vers la nouvelle instance de dog_t, ou NULL en cas d'échec
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog_instance;
+
+	new_dog_instance = malloc(sizeof(dog_t));
+
+	if (new_dog_instance == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog_instance->name = name;
+	new_dog_instance->age = age;
+	new_dog_instance->owner = owner;
+
+	return (new_dog_instance);
+}
---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - Libère la mémoire allouée pour une instance de dog_t
+ * @d: Pointeur vers l'instance de dog_t à libérer
+ *
+ * Description: Cette fonction libère la mémoire allouée pour le nom et le
+ *              propriétaire du chien, ainsi que pour la structure elle-même.
+ */
+
+void free_dog(dog_t *d)
+{
+
+}
