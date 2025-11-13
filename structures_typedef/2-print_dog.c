a1d0501201dab2a3b952550c665bef362a12c163
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-13 13:52:59 +0000

    fix : change argument in printf

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,6 +16,6 @@ void print_dog(struct dog *d)
 		return;
 
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	printf("Age: %lf\n", d->age);
+	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
