d3fa34100b53e19ca33690153cff266803ef2a8b
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-18 08:53:00 +0000

    fix : update code in main and get op

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -17,12 +17,14 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}};
-	int i;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	int i = 0;
+
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
 			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,31 +12,31 @@
 
 int main(int argc, char *argv[])
 {
-	int (*op_func)(int, int);
 	int a, b, result;
+	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	op_func = get_op_func(argv[2]);
 
 	if (op_func == NULL)
 	{
 		printf("Error\n");
-		return (99);
+		exit(99);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	if ((strcmp(argv[2], "/") == 0 && b == 0) ||
-		(strcmp(argv[2], "%") == 0 && b == 0))
+	if ((*(argv[2]) == '/' && b == 0) ||
+		(*(argv[2]) == '%' && b == 0))
 	{
 		printf("Error\n");
-		return (100);
+		exit(100);
 	}
 
 	result = op_func(a, b);
