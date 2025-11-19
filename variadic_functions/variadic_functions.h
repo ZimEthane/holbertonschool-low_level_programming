939882df6f33da7b6744aa2ac0107beca4400a8e
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-11-19 09:20:43 +0000

    fix : change head in .h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
-#ifndef variadic_functions.h
-#define variadic_functions.h
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 
