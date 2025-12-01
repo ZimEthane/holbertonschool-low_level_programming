62cb1cf63a1d69e316a49f983624dfdf6db81c72
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-01 14:54:47 +0000

    fix : add name of function in lists

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list.
- * @head: double pointer to the head of the list.
- * @str: string to be added to the new node.
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: pointer to the head of the list
+ * @str: string to be added to the new node
  *
- * Return: pointer to the new node, or NULL on failure.
+ * Return: address of the new element, or NULL if it failed
  */
 
 list_t *add_node_end(list_t **head, const char *str)
@@ -17,30 +17,24 @@ list_t *add_node_end(list_t **head, const char *str)
 		len++;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
 	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (new_node);
 	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new_node;
-	}
+
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_node;
 
 	return (new_node);
 }
