9e117d147967cfdea43f23294ab33c62f85c8a59
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-01 14:41:34 +0000

    feat : create a function that returns the number of elements in a linked list_t list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to the head of the list.
+ *
+ * Return: the number of nodes in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
