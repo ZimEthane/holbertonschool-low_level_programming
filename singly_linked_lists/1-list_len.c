1304f1a40d0ab19654b85cecd9d987599ea37dfe
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-01 14:42:14 +0000

    fix : add betty documentation

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
+ * list_len - returns the number of elements in a linked list_t list.
  * @h: pointer to the head of the list.
  *
  * Return: the number of nodes in the list.
