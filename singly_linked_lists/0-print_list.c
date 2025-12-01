959868f2638bb97a55b71e672a30610771115825
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-01 09:51:08 +0000

    feat : create folder and file and i add the structure off my code for develop project

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to the head of the list.
+ *
+ * Return: the number of nodes in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	
+}
