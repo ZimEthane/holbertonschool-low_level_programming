c252ef3b7bc4bdfbbc1b24cae7e66884c7295dc0
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-04 13:49:44 +0000

    fix : betty code style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to the
+ * POSIX standard output.
  * @filename: pointer to the name of the file to be read.
  * @letters: number of letters it should read and print.
  *
  * Return: the actual number of letters it could read and print.
  *         If the file can not be opened or read, return 0.
  *         If filename is NULL, return 0.
- *         If write fails or does not write the expected amount of bytes, return 0.
+ *         If write fails or does not write the expected
+ * amount of bytes, return 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, bytes_read, bytes_written;
 	char *buffer;
+
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
