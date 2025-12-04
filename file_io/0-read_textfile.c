3e71767308d1d95cbfe15faf54cbd179deb4133f
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-04 13:46:23 +0000

    fix : betty code style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,26 +11,21 @@
  *         If filename is NULL, return 0.
  *         If write fails or does not write the expected amount of bytes, return 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, bytes_read, bytes_written;
 	char *buffer;
-
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
@@ -38,7 +33,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
@@ -46,7 +40,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	free(buffer);
 	close(fd);
 	return (bytes_written);
